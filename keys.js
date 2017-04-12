f1eaf1f4ab242dd8cedbdcb76339783be1c8f02e
sjdwm33 <samanthadwm@gmail.com>
2017-04-11 21:43:31 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'cefxT0ozOqG8BAXSiaqkjG4OW',
-  consumer_secret: 'dfdcn3ovIVvVcxgZikIdroyQgOISbnZ0mSfbfC2EfTOo6Ztvv2',
-  access_token_key: '113781057-QsowXJAWICYjzKN75R9zDSZEUhnh5exNmBnpo2cQ',
-  access_token_secret: '9ONseg1qlOKkRNlcTTR9z9LpF8cd2k86lEHRhRrKKYCN4',
-}
